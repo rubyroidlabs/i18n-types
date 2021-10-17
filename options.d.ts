1af88733105867739af3dcd94491150f18fea0a8
Pavel Pershko <pavel.pershko@rubyroidlabs.com>
2021-10-17 19:10:14 +0300

    Fix for options typing.

--- options.d.ts
+++ options.d.ts
@@ -1,4 +1,5 @@
-import { ObjectPatternForConcatedKey, TranslationValueByKey } from './nested';
+import { TranslationKeyForLocale } from './locale';
+import { TranslationValueByKey } from './nested';
 import { PluralOptions } from './plural';
 declare type VoidOptionKey = undefined;
 declare type OptionKeys<GTranslation extends string> = GTranslation extends `${string}{{${infer TOption}}}${infer TRestTranslation}` ? Exclude<TOption | OptionKeys<TRestTranslation>, VoidOptionKey> : VoidOptionKey;
@@ -6,6 +7,6 @@ declare type OptionValue = string | number;
 declare type OptionsObj<GKey> = [GKey] extends [string] ? Record<GKey, OptionValue> : {};
 declare type Options<GTranslation extends string> = OptionsObj<OptionKeys<GTranslation>>;
 declare type RawKeyOptions<GKey, Locale> = Options<TranslationValueByKey<GKey, Locale>>;
-declare type KeyOptionsForLocale<GKey extends string, Locale extends ObjectPatternForConcatedKey<GKey>> = TranslationValueByKey<GKey, Locale> extends string ? RawKeyOptions<GKey, Locale> : // @ts-ignore
+declare type KeyOptionsForLocale<Locale, GKey extends TranslationKeyForLocale<Locale>> = TranslationValueByKey<GKey, Locale> extends string ? RawKeyOptions<GKey, Locale> : // @ts-ignore
 RawKeyOptions<`${GKey}_other`, Locale> & PluralOptions;
 export type { KeyOptionsForLocale };
